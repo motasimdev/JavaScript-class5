f28eb84e58b626a2ff9c75aa81a1be76f6fc21df
motasimdev <themotasimdev@gmail.com>
2025-04-28 12:40:20 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let x = 70
+let y = 30
+x = y
+
+console.log(x);
+
+let age = 50
+
+if (age >= 30 && age <= 40) {
+    console.log("young man");
+    
+}
+console.log("not match");
+
+let boyosh = 30
+
+boyosh >= 40 ? console.log("tumi bura") : console.log("tumi young");
+
+
+let z = "amar nam - motasim"
+
+console.log(z.toUpperCase(""));
+
+console.log(z.split(" ")[3]);
+
+
+let heading = document.querySelector("h1")
+let heading2 = document.querySelector(".a")
+
+console.log(heading);
+console.log(heading2.innerHTML);
+
+let heading3 = document.querySelectorAll("h3")
+console.log(heading3);
+
+
+let button = document.querySelector("button")
+
+button.addEventListener("click", function () {
+    heading.style.color= "teal"
+    heading.style.backgroundColor= "#9EFCFF"
+    heading.style.textAlign= "center"
+    heading.style.textTransform= "upperCase"
+    heading.style.borderBottom= "2px solid teal"
+})
+
+
+
+
+
+
